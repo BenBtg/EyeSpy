487332f6cc9f87ab32e559f01d33a890a00a048f
Alexey Strakh <alexey.strakh@microsoft.com>
2018-07-23 16:11:21 -0700

    initial solution structure

---
+++ Client/EyeSpyApp/EyeSpyApp.iOS/AppDelegate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Foundation;
+using UIKit;
+
+namespace EyeSpyApp.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+            ApplyStyle();
+
+            global::Xamarin.Forms.Forms.Init();
+            LoadApplication(new App());
+
+            return base.FinishedLaunching(app, options);
+        }
+
+        private void ApplyStyle()
+        {
+            UINavigationBar.Appearance.Translucent = false;
+            UINavigationBar.Appearance.TintColor = UIColor.White;//.FromRGB(0x8b, 0xc3, 0x4a);
+            UINavigationBar.Appearance.BarTintColor = UIColor.FromRGB(0x8b, 0xc3, 0x4a);
+            UINavigationBar.Appearance.BackgroundColor = UIColor.FromRGB(0x8b, 0xc3, 0x4a);
+
+            UITabBar.Appearance.BackgroundColor = UIColor.FromRGB(0x8b, 0xc3, 0x4a);
+            UITabBar.Appearance.TintColor = UIColor.White;
+        }
+    }
+}
---
+++ Client/EyeSpyApp/EyeSpyApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using EyeSpyApp.Models;
+
+namespace EyeSpyApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
