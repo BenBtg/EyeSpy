129be68aead8cf18a14bfdc4244181135fed0cec
Alexey Strakh <alexey.strakh@microsoft.com>
2018-07-23 17:23:23 -0700

    redo the save button layout at the add new member page

--- Client/EyeSpyApp/EyeSpyApp/Views/NewMemberPage.xaml.cs
+++ Client/EyeSpyApp/EyeSpyApp/Views/NewMemberPage.xaml.cs
@@ -4,6 +4,7 @@ using EyeSpyApp.Models;
 using EyeSpyApp.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using Xamarin.Forms.PlatformConfiguration.iOSSpecific;
 
 namespace EyeSpyApp.Views
 {
@@ -13,6 +14,8 @@ namespace EyeSpyApp.Views
         public NewMemberPage()
         {
             InitializeComponent();
+            On<Xamarin.Forms.PlatformConfiguration.iOS>().SetUseSafeArea(true);
+
             var newMemberContext = (NewMemberViewModel)BindingContext;
             newMemberContext.OnSaveMemberCommandCompleted = async () => await Navigation.PopModalAsync();
         }
